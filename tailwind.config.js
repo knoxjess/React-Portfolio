eafdc494e402336881f0d2c63ed3b8d0cc421122
knoxjess <110634800+knoxjess@users.noreply.github.com>
2023-05-05 21:20:50 -0500

    <added packages/readme/.gitignore>

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        "deep-blue": "#010026",
+        blue: "#2CBCE9",
+        red: "#DC4492",
+        yellow: "#FDCC49",
+        grey: "#ededed",
+        "dark-grey": "#757575",
+        "opaque-black": "rgba(0,0,0,0.35)",
+      },
+
+      fontFamily: {
+        playfair: ["Playfair Display", "serif"],
+        opensans: ["Open Sans", "sans-serif"],
+      },
+      content: {
+        person1: "url('./assets/person-1.png')",
+        person2: "url('./assets/person-2.png')",
+        person3: "url('./assets/person-3.png')",
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
